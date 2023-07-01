50f7f8b9cf624f5260f337add2ac58e89b9ac8c5
Thian Hau Ooi <thian-hau.ooi@keysight.com>
2023-07-01 20:19:09 +0800

    Home component get user list api

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { User } from 'src/app/models/users';
+import { UserApiService } from 'src/app/services/user-api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  private userList!: User[];
+  private unsubscribe = new Subject<void>();
 
+  public get UserList(): User[] {
+    return this.userList;
+  }
+
+  constructor(
+    private userApiService: UserApiService,
+  ) {
+   
+   }
+
+  ngOnInit(): void {
+    this.GetUserList();
+    
+    this.userApiService.User$.pipe(takeUntil(this.unsubscribe)).subscribe(u => {
+      this.userList = u;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
+
+  public GetUserList(): void {
+    this.userApiService.GetAllUser().pipe(takeUntil(this.unsubscribe)).subscribe({
+      next: (u) => {
+        this.userApiService.UpdateUser(u);
+      }
+    });
+  }
 }
